9ac993e85d85950721fbbe2d51f9a2e265d4231e
jdauliac <jdauliac@sellsy.com>
2023-12-03 21:50:18 +0100

    fix: remove install useless install options

--- lua/phpactor.lua
+++ lua/phpactor.lua
@@ -18,7 +18,6 @@ phpactor.AVAILABLE_RPC = {
   "new_class",
   "status",
   "transform",
-  "update",
   "lsp/status",
   "lsp/reindex",
   "lsp/debug/config",
@@ -30,11 +29,6 @@ phpactor.AVAILABLE_RPC = {
 function phpactor.setup(options)
   config.setup(options)
 
-  vim.api.nvim_create_autocmd("VimEnter", {
-    pattern = "*",
-    callback = phpactor.check_install,
-  })
-
   if config.options.lspconfig.enabled then
     require("lspconfig").phpactor.setup(vim.tbl_deep_extend("force", {
       cmd = {
@@ -42,24 +36,6 @@ function phpactor.setup(options)
         "language-server",
       },
     }, config.options.lspconfig.options))
-  end
-end
-
-function phpactor.check_install()
-  if config.options.install.check_on_startup == "none" then
-    return
-  end
-
-  if config.options.install.check_on_startup == "daily" then
-    if vim.g.PHPACTOR_LAST_CHECK ~= nil and vim.g.PHPACTOR_LAST_CHECK == os.date("%x") then
-      return
-    end
-
-    vim.g.PHPACTOR_LAST_CHECK = os.date("%x")
-  end
-
-  if not require("phpactor.check_install")() then
-    vim.notify("PhpActor is outdated\nrun `:PhpActor update`", vim.log.levels.INFO, { title = "PhpActor" })
   end
 end
 
--- lua/phpactor/config.lua
+++ lua/phpactor/config.lua
@@ -4,13 +4,10 @@ config.options = {}
 
 local default_values = {
   install = {
-    path = vim.fn.stdpath("data") .. "/opt/",
     branch = "master",
-    bin = nil,
+    bin = "phpactor",
     php_bin = "php",
     composer_bin = "composer",
-    git_bin = "git",
-    check_on_startup = "none",
   },
   lspconfig = {
     enabled = true,
@@ -20,24 +17,6 @@ local default_values = {
 
 function config.setup(options)
   config.options = vim.tbl_deep_extend("force", default_values, options or {})
-
-  -- add trailing space if necessary
-  if nil == string.match(config.options.install.path, "/$") then
-    config.options.install.path = config.options.install.path .. "/"
-  end
-
-  if nil == config.options.install.bin then
-    config.options.install.bin = config.options.install.path .. "phpactor/bin/phpactor"
-  end
-
-  if not vim.tbl_contains({ "daily", "always", "none" }, config.options.install.check_on_startup) then
-    vim.notify(
-      "Invalid value for `check_on_startup` option\nAccepted values : daily, always, none",
-      vim.log.levels.ERROR,
-      { title = "PhpActor" }
-    )
-    config.options.install.check_on_startup = "none"
-  end
 end
 
 return config
